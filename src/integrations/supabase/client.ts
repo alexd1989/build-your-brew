d3e3ac48dc0656c835dbc3ac7b28fde760033d90
alexd1989 <alex@alexdranca.com>
2025-08-12 18:40:41 +0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,7 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = "https://oyyvmabeweaswyiexphe.supabase.co";
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_I48BauTWWTz3Mdb6GniYOg_b1sIziR7";
+
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
